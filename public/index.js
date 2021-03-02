08104c8f969c31896348118216bba3e4ebecabf3
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-03-02 15:09:09 -0500

    added err handling to serviceworker reg

--- public/index.js
+++ public/index.js
@@ -4,6 +4,9 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
       .then((reg) => {
         console.log('Service worker registered.', reg);
+      }, (err) => {
+        // registration failed
+        console.log('ServiceWorker registration failed: ', err);
       });
   });
 }
